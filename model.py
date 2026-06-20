88cdcad37da50b19575c8c66b7e7f35fd49e6a21
talos <95100399+stollthbel@users.noreply.github.com>
2026-06-20 18:45:56 -0400

    Step 12: build_text_dataset

--- model.py
+++ model.py
@@ -122,8 +122,12 @@ def format_all_examples(examples):
     """Format each instruction/response dict into a training string."""
     return [format_instruction_example(example) for example in examples]
 
-# Step 12 - build_text_dataset (not yet solved)
-# TODO: implement
+# Step 12 - build_text_dataset
+from datasets import Dataset
+
+def build_text_dataset(texts):
+    """Wrap a list of training strings in a HF Dataset with a 'text' column."""
+    return Dataset.from_dict({"text": texts})
 
 # Step 13 - tokenize_text (not yet solved)
 # TODO: implement
